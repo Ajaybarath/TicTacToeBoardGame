e814cdc8c0820d361c37b94df8cf55bdafd4b28b
ajaybarath <ajaybarath3@gmail.com>
2021-09-06 09:23:09 +0530

    ADDED: function to brint the board

--- src/com/bridgeLabz/ticTacToe/TicTacToeGame.java
+++ src/com/bridgeLabz/ticTacToe/TicTacToeGame.java
@@ -26,6 +26,7 @@ public class TicTacToeGame {
 		System.out.println("Enter your position player 2");
 		ticTacToe.play(s.nextInt(), s.nextInt(), 2);
 		
+		ticTacToe.printBoard();
 		
 	}
 	
@@ -64,6 +65,17 @@ public class TicTacToeGame {
 		
 		return ' ';
 		
+	}
+	
+	void printBoard() {
+		for (int i=0;i<3;i++) {
+			for (int j=0;j<3;j++) {
+				if (board[i][j] == ' ')
+					System.out.print(" ");
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 	
 	
